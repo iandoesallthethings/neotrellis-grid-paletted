0e40ba5b62acf4e3af37ab538a1683d4f4c55433
iandoesallthethings <iandoesallthethings@gmail.com>
2023-07-21 01:41:12 -0400

    Modify for the itsybitsy and resolve compilation errors.

--- MonomeSerialDevice.cpp
+++ MonomeSerialDevice.cpp
@@ -247,7 +247,7 @@ void MonomeSerialDevice::processSerial() {
                 if (i < deviceID.length()) {
                   Serial.write(deviceID[i]);
                 } else {
-                  Serial.write(0x00);
+                  Serial.write((byte)0x00);
                 }
             }
             break;
